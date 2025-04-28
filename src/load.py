cde8cdf27a775dbc347b96a6eadd7569425c1c81
KevinFernandez21 <kdfernan@espol.edu.ec>
2025-04-28 17:59:17 -0500

    load

--- src/load.py
+++ src/load.py
@@ -12,7 +12,5 @@ def load(data_frames: Dict[str, DataFrame], database: Engine):
         and values as the dataframes.
     """
     # TODO: Implement this function. For each dataframe in the dictionary, you must
-    # use pandas.Dataframe.to_sql() to load the dataframe into the database as a
-    # table.
-    # For the table name use the `data_frames` dict keys.
-    raise NotImplementedError
+    for  table_name,df in data_frames.items():
+        df.to_sql(table_name, con=database, if_exists='replace', index=False) 
